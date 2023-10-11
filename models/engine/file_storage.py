c01a0dee4d22c28ae7105b154198164b8d498bd8
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-10-11 08:51:01 -0700

    AirBnB_console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,12 +29,12 @@ class FileStorage:
     def classes(self):
         """Returns a dictionary of valid classes and their references"""
         from models.base_model import BaseModel
-        from models.user import User
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.place import Place
-        from models.review import Review
+        from models.User import User
+        from models.State import State
+        from models.City import City
+        from models.Amenity import Amenity
+        from models.Place import Place
+        from models.Review import Review
 
         classes = {"BaseModel": BaseModel,
                    "User": User,
